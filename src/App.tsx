e0f1673ff7c98f58e75473618992a2ac4aa58cf5
Kimberg23 <imbergk23@gmail.com>
2026-04-20 14:11:49 -0500

    Load email from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,27 @@ const App = () => {
     [],
   );
 
+    useEffect(
+    () => {
+      const loadEmail = async () => {
+
+        const result = await localforage.getItem<string>("email") ?? "";
+
+        if (!ignore) {
+          setEmailInDialog(result);
+        }
+      }
+
+      let ignore = false;
+      loadEmail();
+
+      return () => {
+        ignore = true;
+      }
+    },
+    [],
+  );
+
   //
   // Calculate state and other funcs...
   //
