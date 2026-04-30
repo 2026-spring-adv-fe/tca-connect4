7ab779ede2f047dae0b2aed0dac208a2ac2b15ef
Kimberg23 <imbergk23@gmail.com>
2026-04-30 08:11:12 -0500

    Added cancel button next to save email

--- src/App.tsx
+++ src/App.tsx
@@ -299,6 +299,13 @@ const App = () => {
             <div className="modal-action">
               <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
+                <button
+                  className="btn btn-ghost mr-2"
+                  type="button"
+                  onClick={() => emailDialog.current?.close()}
+                >
+                  Cancel
+                </button>
                 <button 
                   className="btn"
                   onClick={
